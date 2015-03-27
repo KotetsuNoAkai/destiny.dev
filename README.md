7710284d2bec1688b151ab8c7304fdffd3e82a4d
KotetsuNoAkai <akainoken@gmail.com>
2015-03-27 13:36:09 +0100

    Instalación base de Symfony
    Instalacion de los bundles:
    - Ladybug.
    - Doctrine extensions.
    - Fixtures.
    - FOS user (desactivado).
    
    Creación de DestinyAppBundle

---
+++ README.md
@@ -0,0 +1,4 @@
+destiny.dev
+===========
+
+A Symfony project created on March 27, 2015, 1:18 pm.
---
+++ src/Destiny/AppBundle/DestinyAppBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Destiny\AppBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class DestinyAppBundle extends Bundle
+{
+}

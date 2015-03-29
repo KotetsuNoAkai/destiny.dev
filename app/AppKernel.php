d1ddd33306edaee9fbb3361cf5b6589608107f1f
KotetsuNoAkai <akainoken@gmail.com>
2015-03-29 23:06:39 +0200

    29/03/2015
     BackendController:
    
    - Ahora, la subida de archivos solo es obligatoria en la creacion de la entidad.
    - Añadida las traduciones a los mensajes FLASH.
    - En el caso de que en el REPOSITORIO de la entidad exista auna funcion llamada GETONE, se llama a esta, en vez de a FINDONEBYSLUG.
    
    IdiomasType:
    -Añadida las traducciones a la etiqueta.

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -1,7 +1,7 @@
 <?php
 
-use Symfony\Component\HttpKernel\Kernel;
 use Symfony\Component\Config\Loader\LoaderInterface;
+use Symfony\Component\HttpKernel\Kernel;
 
 class AppKernel extends Kernel
 {
@@ -27,6 +27,7 @@ class AppKernel extends Kernel
             $bundles[] = new Sensio\Bundle\DistributionBundle\SensioDistributionBundle();
             $bundles[] = new Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle();
 	        $bundles[] = new RaulFraile\Bundle\LadybugBundle\RaulFraileLadybugBundle();
+
         }
 
         return $bundles;
